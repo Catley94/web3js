1df35c1902a43fff80bab3303fac57ff9d1e869e
Samuel Catley <scp94373@gmail.com>
2019-03-09 17:23:49 +0000

    initial commit
    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: ""
+    }
+  }
+  componentWillMount() {
+    this.loadBlockchainData() 
+    }
+  
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType();
+    //test console
+    console.log("network:", network)
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0] })
+    
+    }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Your Account: {this.state.account}</p>
         </header>
       </div>
     );
